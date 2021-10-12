1a218ce6440d39a40d4a90a91646ecfa60983c2f
Swilder-M <poxiaobbs@gmail.com>
2021-10-12 15:04:50 +0800

    remove index tmp

--- src/algolia_helper.py
+++ src/algolia_helper.py
@@ -12,22 +12,15 @@ class AlgoliaHelper:
     def __init__(self, app_id, api_key, index_name, index_name_tmp, settings, query_rules, current_product, current_version):
         self.algolia_client = SearchClient.create(app_id, api_key)
         self.index_name = index_name
-        self.index_name_tmp = index_name_tmp
         self.algolia_index = self.algolia_client.init_index(self.index_name)
-        self.algolia_index_tmp = self.algolia_client.init_index(
-            self.index_name_tmp)
         # self.algolia_client.copy_rules(
         #     self.index_name,
         #     self.index_name_tmp
         # )
-        self.algolia_client.copy_index(
-            self.index_name,
-            self.index_name_tmp
-        )
-        self.algolia_index_tmp.set_settings(settings)
+        self.algolia_index.set_settings(settings)
 
         if len(query_rules) > 0:
-            self.algolia_index_tmp.save_rules(query_rules, True, True)
+            self.algolia_index.save_rules(query_rules, True, True)
 
         # for emqx docs
         index_facet_filters = []
@@ -38,7 +31,7 @@ class AlgoliaHelper:
             index_facet_filters.append(f'version:{current_version}')
 
         if index_facet_filters:
-            self.algolia_index_tmp.delete_by({
+            self.algolia_index.delete_by({
               'facetFilters': index_facet_filters
             })
 
@@ -47,7 +40,7 @@ class AlgoliaHelper:
         record_count = len(records)
 
         for i in range(0, record_count, 50):
-            self.algolia_index_tmp.save_objects(records[i:i + 50])
+            self.algolia_index.save_objects(records[i:i + 50])
 
         color = "96" if from_sitemap else "94"
 
@@ -60,12 +53,12 @@ class AlgoliaHelper:
         for _, value in list(synonyms.items()):
             synonyms_list.append(value)
 
-        self.algolia_index_tmp.save_synonyms(synonyms_list)
+        self.algolia_index.save_synonyms(synonyms_list)
         print(
             '\033[94m> DocSearch: \033[0m Synonyms (\033[93m{} synonyms\033[0m)'.format(
                 len(synonyms_list)))
 
     def commit_tmp_index(self):
         """Overwrite the real index with the temporary one"""
-        # print("Update settings")
-        self.algolia_client.move_index(self.index_name_tmp, self.index_name)
+        print("Update settings")
+        # self.algolia_client.move_index(self.index_name_tmp, self.index_name)
