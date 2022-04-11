5adeb79ac346e56d1583437a74ab449191b854c8
Swilder-M <poxiaobbs@gmail.com>
2022-04-11 10:26:03 +0800

    modify index name

--- src/index.py
+++ src/index.py
@@ -123,12 +123,15 @@ if __name__ == '__main__':
 
     config_dict = json.load(open(f'{config_name}.json', 'r'))
     if product == 'broker':
+        config_dict['index_name'] = 'emqx'
         config_dict['sitemap_urls'] = [f'https://www.emqx.io/docs/sitemap_{version}.xml']
     elif product == 'nanomq':
+        config_dict['index_name'] = 'nanomq'
         config_dict['sitemap_urls'] = [f'https://nanomq.io/docs/sitemap_{version}.xml']
     elif product == 'neuron':
         config_dict['sitemap_urls'] = [f'https://neugates.io/docs/sitemap_{version}.xml']
     elif product == 'ekuiper':
+        config_dict['index_name'] = 'ekuiper'
         config_dict['sitemap_urls'] = [f'https://ekuiper.org/docs/sitemap_{version}.xml']
     elif product == 'hstreamdb':
         config_dict['sitemap_urls'] = [f'https://hstream.io/docs/sitemap_{version}.xml']
